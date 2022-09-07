f325e1b9bac97d18b8a15e49800fff2c8cecda13
reichert621 <reichertjalex@gmail.com>
2022-09-07 10:19:05 -0400

    Smooth things out a bit

--- components/ContractDebugger.tsx
+++ components/ContractDebugger.tsx
@@ -16,6 +16,7 @@ import {
 } from '@/utils/etherscan';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import {Spinner} from '@/components/Icon';
 
 const getHoverColorByNetwork = (network: string) => {
   switch (network) {
@@ -265,8 +266,23 @@ export const ContractDebugger = ({
   };
 
   if (isLoading) {
-    // TODO: handle loading state
-    return null;
+    return (
+      <div className="flex min-h-full flex-auto flex-col bg-gray-50">
+        <Head>
+          <title>Contract debugger</title>
+          <meta name="description" content="Contract debugger" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <NavBar />
+
+        <main className="flex flex-1 flex-col items-center justify-center">
+          <Spinner className="h-8 w-8 animate-spin text-gray-700 opacity-20" />
+        </main>
+
+        <Footer />
+      </div>
+    );
   }
 
   if (!contract && !account) {
@@ -301,8 +317,20 @@ export const ContractDebugger = ({
   }
 
   if (!contract) {
-    // TODO: if no contract, show error/info state
-    return null;
+    // TODO: handle this better
+    return (
+      <div className="flex min-h-full flex-auto flex-col bg-gray-50">
+        <Head>
+          <title>Contract debugger</title>
+          <meta name="description" content="Contract debugger" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <NavBar />
+        <main className="flex flex-1 flex-col items-center justify-center"></main>
+        <Footer />
+      </div>
+    );
   }
 
   const defaultTitle = metadata?.name
@@ -383,26 +411,7 @@ export const ContractDebugger = ({
                   onClick={handleAttemptSyncAbi}
                 >
                   {isSyncing && (
-                    <svg
-                      className="-ml-1 mr-2 h-4 w-4 animate-spin text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
+                    <Spinner className="-ml-1 mr-2 h-4 w-4 animate-spin text-white" />
                   )}
                   {isSyncing ? 'Syncing...' : 'Sync with Etherscan'}
                 </button>
